b7f2fffcb6c3bad409f4d26877f0a70e3736f337
Aji Guna <gunaitsugo@gmail.com>
2022-03-04 22:26:41 +0700

    change handler post note

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -16,7 +16,7 @@ class NotesHandler {
     try {
       this._validator.validateNotePayload(request.payload);
       const { title = 'untitled', body, tags } = request.payload;
-      this._service.addNote({ title, body, tags });
+      // this._service.addNote({ title, body, tags });
 
       const noteId = this._service.addNote({ title, body, tags });
 
